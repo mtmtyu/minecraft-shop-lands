f9dbefad51cf9ad7fda5c211e283cfe15153fffe
mtmtyu <81827724+mtmtyu@users.noreply.github.com>
2025-05-24 01:21:58 +0900

    初期設定ファイルとプラグインの基本構造を追加

---
+++ src/main/java/com/mochiserver/minecraftShopLands/MinecraftShopLands.java
@@ -0,0 +1,17 @@
+package com.mochiserver.minecraftShopLands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftShopLands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
